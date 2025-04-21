1ffff9769f2d6e49f18f24eaa3ae903346a85684
Manzoor Strange <strangemia7001@gmail.com>
2025-04-21 14:29:45 +0500

    added database connection and server connection config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import app from "./app.js";
-
+import connectDB from "./db/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT;
 // here database connection with server connection
-
-
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("server started successfully at PORT:", PORT);
+    });
+    app.on("error", (error) => {
+      console.log(error);
+      throw error;
+    });
+  })
+  .catch((error) => {
+    console.log("ERROR: ", error.message);
+  });
